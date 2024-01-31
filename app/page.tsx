d2833817dd9413e32bd029d98b822033a7cecc0c
ChrisKnowsDev <chrisharley81@gmail.com>
2024-01-31 10:03:36 -0500

    added metadata title and description for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+import { Metadata } from 'next'
+
+export const metaData: Metadata = {
+  title: 'chrisCooks',
+  description: 'A cooking blog for chef Chris'
+}
 
 export default function Home() {
   return (
-    <main className="text-4xl">
-      <h1>Cooking with Chris</h1>
+    <main className="text-4xl p-4 max-w-xl mx-auto">
+      <header>
+        <h1>Cooking with Chris</h1>
+      </header>
+      <section>
+        <p className="text-base">blog post will go here</p>
+      </section>
     </main>
   )
 }
