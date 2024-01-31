ee2d37f04d1711755c0eb7ac1b81e5b977f8fcd9
ChrisKnowsDev <chrisharley81@gmail.com>
2024-01-31 13:32:08 -0500

    connected themealdb API to display recipes

--- components/blog-post-preview.tsx
+++ components/blog-post-preview.tsx
@@ -1,24 +1,25 @@
 import Image from 'next/image'
-import image1 from '../public/images/alex-munsell.jpg'
+import Link from 'next/link'
+import { MealType } from '@/utils/types'
 
-export default function BlogPostPreview() {
+export default function BlogPostPreview({ meals }: any) {
   return (
-    <div className="text-base mt-4">
-      <Image
-        src={image1}
-        alt="picture of a pork chop with potatoes and broccoli"
-        className="rounded-sm"
-      />
-      <header className="mt-2 mb-2">
-        <p className="text-lg font-bold">Pork Chop Perfection</p>
-        <p className="text-sm">Aug 08, 2023</p>
-      </header>
-      <p className="text-slate-500">
-        Indulge in a harmonious blend of flavors with our Pork Chop, Potatoes,
-        and Broccoli recipe. Juicy pork chops, golden potatoes, and crisp
-        broccoli create a visually appealing and satisfying dining experience,
-        balancing savory, hearty, and wholesome elements on your plate.
-      </p>
-    </div>
+    <ul className="">
+      {meals.map((meal: MealType) => (
+        <li key={meal.idMeal}>
+          <Link href={`/meals/${meal.idMeal}`}>
+            <div className=" relative h-48 mb-4">
+              <Image
+                src={meal.strMealThumb}
+                alt={meal.strMeal}
+                fill
+                sizes="(max-width: 768px) 100vw,  (max-width: 1200px) 50vw"
+                className="rounded-md object-cover"
+              />
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
