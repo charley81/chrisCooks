630f03a73e52d8e88efb7eb69118a174a1ded309
ChrisKnowsDev <chrisharley81@gmail.com>
2024-02-01 16:06:36 -0500

    connected datocms and fetched inital data in homepage

--- app/meals/error.tsx
+++ app/meals/error.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-export default function error(error) {
+export default function error(error: any) {
   return (
     <div className="text-2xl p-4 max-w-xl mx-auto">
       <p>The following error occurred: {error.error.message}</p>
