da0ffb4401f32e280ddfa5ece1f8c2a557e00f20
ChrisKnowsDev <chrisharley81@gmail.com>
2024-02-05 10:59:03 -0500

    added links file for navbar

---
+++ utils/links.tsx
@@ -0,0 +1,15 @@
+import { CookingPot, Utensils } from 'lucide-react'
+import { NavLink } from './types'
+
+const links: NavLink[] = [
+  {
+    href: 'recipes',
+    label: 'recipes',
+    icon: <CookingPot />
+  },
+  {
+    href: '/api-recipes',
+    label: 'api-recipes',
+    icon: <Utensils />
+  }
+]
