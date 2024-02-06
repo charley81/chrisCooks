f31a63d75876247f41d5c3740c0fbec5d6695779
ChrisKnowsDev <chrisharley81@gmail.com>
2024-02-06 11:31:04 -0500

    fixed nav links not showing on desktop issue

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,14 +4,16 @@ import links from '@/utils/links'
 import LinksDropdown from './links-dropdown'
 import { ThemeToggle } from './theme-toggle'
 import { UserButton } from '@clerk/nextjs'
+import Logo from './logo'
 
 export default function Navbar() {
   return (
     <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
       <div>
+        <Logo />
         <LinksDropdown />
       </div>
-      <ul className="flex items-center gap-x-4 hidden">
+      <ul className="w-full hidden lg:flex lg:justify-center lg:gap-x-4">
         {links.map(link => (
           <li key={link.label}>
             <Link href={link.href}>{link.label}</Link>
