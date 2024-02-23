55bd1adb1467e62bc374fe891ffd3732a4c9acf9
ChrisKnowsDev <chrisharley81@gmail.com>
2024-02-23 12:32:30 -0500

    search form initial setup

--- components/form/search-form.tsx
+++ components/form/search-form.tsx
@@ -1,3 +1,45 @@
+'use client'
+
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { usePathname, useParams, useSearchParams } from 'next/navigation'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import { CategoryTypes } from '@/types/my-recipes/types'
+
 export default function SearchForm() {
-  return <div>SearchForm</div>
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const search = formData.get('search') as string
+    const category = formData.get('category') as string
+    console.log(search, category)
+  }
+
+  return (
+    <form
+      className="mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4"
+      onSubmit={handleSubmit}
+    >
+      <Input type="text" placeholder="Search recipes" name="search" />
+      <Select name="category">
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {['all', ...Object.values(CategoryTypes)].map(category => (
+            <SelectItem key={category} value={category}>
+              {category}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Button type="submit">Search</Button>
+    </form>
+  )
 }
