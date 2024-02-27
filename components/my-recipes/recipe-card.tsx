cfaa06c2e4e44ec709dc0fc6f8999908419bb2f1
ChrisKnowsDev <chrisharley81@gmail.com>
2024-02-27 18:17:38 -0500

    initial recipe-card main setup

--- components/my-recipes/recipe-card.tsx
+++ components/my-recipes/recipe-card.tsx
@@ -1,5 +1,35 @@
 import { MyRecipeType } from '@/types/my-recipes/types'
+import { CalendarDays, Pencil, Trash2 } from 'lucide-react'
+import Link from 'next/link'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { Separator } from '../ui/separator'
+import { Button } from '../ui/button'
+import { Badge } from '../ui/badge'
+import RecipeInfo from './recipe-info'
+import DeleteRecipeBtn from './delete-recipe-btn'
 
 export default function RecipeCard({ recipe }: { recipe: MyRecipeType }) {
-  return <div>RecipeCard</div>
+  return (
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{recipe.title}</CardTitle>
+        <CardDescription>{recipe.category}</CardDescription>
+      </CardHeader>
+      <Separator className="mt-4" />
+      <CardContent></CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size="sm">
+          <Link href={`/my-recipes/${recipe.id}`}>edit</Link>
+        </Button>
+        <DeleteRecipeBtn />
+      </CardFooter>
+    </Card>
+  )
 }
