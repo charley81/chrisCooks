9eb73d3032505f099503d55cc597f8d73a4cb27c
ChrisKnowsDev <chrisharley81@gmail.com>
2024-03-02 14:26:29 -0500

    initial hover modal funtionality on my-recipe card complete

--- components/my-recipes/recipe-card.tsx
+++ components/my-recipes/recipe-card.tsx
@@ -12,38 +12,59 @@ import {
 import { Button } from '../ui/button'
 import DeleteRecipeBtn from './delete-recipe-btn'
 import { truncateText } from '@/utils/helpers'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger
+} from '@/components/ui/hover-card'
 
 export default function RecipeCard({ recipe }: { recipe: MyRecipeType }) {
   const date = new Date(recipe.createdAt).toLocaleDateString()
+  const formattedDescription = recipe.description.split('\n\n')
 
   return (
-    <Card className="bg-card flex flex-col">
-      {/* header */}
-      <CardHeader>
-        <CardTitle>{recipe.title}</CardTitle>
-        <CardDescription>{recipe.category}</CardDescription>
-      </CardHeader>
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <Card className="bg-card flex flex-col static">
+          {/* header */}
+          <CardHeader>
+            <CardTitle>{recipe.title}</CardTitle>
+            <CardDescription>{recipe.category}</CardDescription>
+          </CardHeader>
 
-      <CardContent>
-        <p className="text-muted-foreground">
-          {truncateText(recipe.description, 'lg')}
-        </p>
-      </CardContent>
+          <CardContent>
+            <p className="text-muted-foreground">
+              {truncateText(recipe.description, 'md')}
+            </p>
+          </CardContent>
 
-      <CardFooter className="flex justify-between flex-auto">
-        <p className="flex items-center gap-2">
-          <CalendarDays />
-          {date}
-        </p>
-        <div className="flex gap-4 items-center">
-          <Button asChild size="sm">
-            <Link href={`/my-recipes/${recipe.id}`}>
-              <Pencil />
-            </Link>
-          </Button>
-          <DeleteRecipeBtn id={recipe.id} />
-        </div>
-      </CardFooter>
-    </Card>
+          <CardFooter className="flex justify-between flex-auto">
+            <p className="flex items-center gap-2">
+              <CalendarDays />
+              {date}
+            </p>
+            <div className="flex gap-4 items-center">
+              <Button asChild size="sm">
+                <Link href={`/my-recipes/${recipe.id}`}>
+                  <Pencil />
+                </Link>
+              </Button>
+              <DeleteRecipeBtn id={recipe.id} />
+            </div>
+          </CardFooter>
+        </Card>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-96" sideOffset={-500}>
+        <h3 className="text-2xl font-bold">{recipe.title}</h3>
+        {truncateText(recipe.description, 'lg')
+          .split('\n\n')
+          .map(description => (
+            <p key={description} className="my-8 text-muted-foreground">
+              {description}
+            </p>
+          ))}
+        <Button>View Page</Button>
+      </HoverCardContent>
+    </HoverCard>
   )
 }
--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -1,9 +1,9 @@
 export function truncateText(text: string, size?: 'sm' | 'md' | 'lg') {
   let maxLength = 15
   if (size === 'md') {
-    maxLength = 48
-  } else if (size === 'lg') {
     maxLength = 88
+  } else if (size === 'lg') {
+    maxLength = 350
   }
 
   return text.length > maxLength ? text.slice(0, maxLength - 1) + '...' : text
